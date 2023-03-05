9901a7b30676d65fcd5ddb107016b72492ccbf25
qopu <53898007+qopu@users.noreply.github.com>
2023-03-05 23:25:18 +0300

    Fix

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ class User:
 
 
 class SecurityHandler:
-    def __init__(self):
-        pass
-
     def handle_request(self, user: User) -> None:
         raise NotImplementedError()
 
+
 class SecurityHandlerChainPart(SecurityHandler):
     __next_security_handler: SecurityHandler
+
+    def __init__(self):
+        self.__next_security_handler = NoneSecurityHandler()
 
     def set_next(self, next_handler: SecurityHandler) -> None:
         self.__next_security_handler = next_handler
@@ -25,15 +26,17 @@ class SecurityHandlerChainPart(SecurityHandler):
     def handle_request(self, user: User) -> None:
         return self.__next_security_handler.handle_request(user=user)
 
+
 class NoneSecurityHandler(SecurityHandler):
-    def handle_request(self, user: User) -> None:
-        pass
+    def handle_request(self, user: User):
+        return "Доступ разрешён!"
+
 
 class AuthenticationHandler(SecurityHandlerChainPart):
-    __valid_credentials: dict = { "user": "user",
-                                  "admin": "admin" }
+    __valid_credentials: dict = {"user": "user",
+                                 "admin": "admin"}
 
-    def handle_request(self, user: User) -> None:
+    def handle_request(self, user: User):
         is_user_valid = user.username in self.__valid_credentials and \
                         user.password == self.__valid_credentials[user.username]
         if is_user_valid:
@@ -41,18 +44,49 @@ class AuthenticationHandler(SecurityHandlerChainPart):
         return "Ошибка аутентификации"
 
 
-
 class AuthorizationHandler(SecurityHandlerChainPart):
-    __minimum_access_level: int
+    __minimum_access_level: int = 3
 
-    def handle_request(self, user: User) -> None:
+    def handle_request(self, user: User):
         is_user_authorized = self.__minimum_access_level < user.access_level
         if is_user_authorized:
             return super().handle_request(user=user)
         return "Ошибка авторизации"
 
 
-class AuditHandler(SecurityHandlerChainPart):
-    def handle_request(self, user: User) -> None:
-        print(f"Пользователь {user} произвёл авторизацию")
+class AccountingHandler(SecurityHandlerChainPart):
+    def handle_request(self, user: User):
+        with open("logs.txt", "a") as file:
+            file.writelines(f"User \"{user.password}\" authorized with access level {user.access_level}\n")
         return super().handle_request(user=user)
+
+
+def do_tests():
+    valid_user = User(username="user",
+                      password="user",
+                      access_level=4)
+    invalid_user = User(username="hacker",
+                        password="iWantToGetAccessSoBad",
+                        access_level=5)
+    low_access_user = User(username="user",
+                           password="user",
+                           access_level=1)
+    admin = User(username="admin",
+                 password="admin",
+                 access_level=999)
+
+    authentication = AuthenticationHandler()
+    authorization = AuthorizationHandler()
+    accounting = AccountingHandler()
+
+    authentication.set_next(authorization)
+    authorization.set_next(accounting)
+
+    print(f"Valid user: {authentication.handle_request(user=valid_user)}")
+    print(f"Invalid user: {authentication.handle_request(user=invalid_user)}")
+    print(f"User with low access level: {authentication.handle_request(user=low_access_user)}")
+    print(f"Admin: {authentication.handle_request(user=admin)}")
+
+
+if __name__ == '__main__':
+    do_tests()

a6e8bb39792da5223a0bf0528b17b2635a9636f1
SweTaLooD <124892933+SweTaLooD@users.noreply.github.com>
2023-03-14 18:17:05 +0300

    ,
    .

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-print("hello")
+class User:
+    username: str
+    password: str
+    access_level: int
+
+    def __init__(self, username: str, password: str, access_level: int):
+        pass
+
+
+class SecurityHandler:
+    def handle_request(self, user: User):
+        pass
+
+
+class NoneSecurityHandler(SecurityHandler):
+    def handle_request(self, user: User):
+        pass
+
+
+class SecurityHandlerChainPart(SecurityHandler):
+    next_security_handler: SecurityHandler
+
+    def set_next(self, next_handler: SecurityHandler):
+        pass
+
+    def handle_request(self, user: User):
+        pass
+
+
+class AuthenticationHandler(SecurityHandlerChainPart):
+    __valid_credentials: dict
+
+    def handle_request(self, user: User):
+        pass
+
+
+class AuthorizationHandler(SecurityHandlerChainPart):
+    __minimum_access_level: int
+
+    def handle_request(self, user: User):
+        pass
+
+
+class AuditHandler(SecurityHandlerChainPart):
+    def handle_request(self, user: User):
+        pass
+    
